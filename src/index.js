d41a25e935ddb9c6ece76b7ba0ec0bded5276c83
sunmix07 <sunmix_07@bk.ru>
2018-11-09 13:04:17 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -15,12 +15,14 @@ class Gravatar extends React.Component {
     className: PropTypes.string,
     protocol: PropTypes.string,
     style: PropTypes.object,
+    customAvatar: PropTypes.string,
   }
   static defaultProps = {
     size: 50,
     rating: 'g',
     default: 'retro',
     protocol: '//',
+    customAvatar: '',
   }
 
   render() {
@@ -38,19 +40,26 @@ class Gravatar extends React.Component {
       d: this.props.default,
     })
 
+    // get the customAvatar url
+    const { customAvatar } = this.props
+
+
     // Gravatar service currently trims and lowercases all registered emails
-    const formattedEmail = ('' + this.props.email).trim().toLowerCase();
+    const formattedEmail = (`${this.props.email}`).trim().toLowerCase()
 
     let hash
-    if (this.props.md5) {
-      hash = this.props.md5
-    } else if (typeof this.props.email === 'string') {
-      hash = md5(formattedEmail, {encoding: "binary"})
-    } else {
-      console.warn(
-        'Gravatar image can not be fetched. Either the "email" or "md5" prop must be specified.'
+    // if not on customAvatar Mode
+    if (customAvatar === '') {
+      if (this.props.md5) {
+        hash = this.props.md5
+      } else if (typeof this.props.email === 'string') {
+        hash = md5(formattedEmail, { encoding: 'binary' })
+      } else {
+        console.warn(
+        'Gravatar image can not be fetched. Either the "email" or "md5" prop must be specified.',
       )
-      return (<script />)
+        return (<script />)
+      }
     }
 
     const src = `${base}${hash}?${query}`
@@ -68,9 +77,10 @@ class Gravatar extends React.Component {
       className = `${className} ${this.props.className}`
     }
 
+
     // Clone this.props and then delete Component specific props so we can
     // spread the rest into the img.
-    let { ...rest } = this.props
+    const { ...rest } = this.props
     delete rest.md5
     delete rest.email
     delete rest.protocol
@@ -79,6 +89,25 @@ class Gravatar extends React.Component {
     delete rest.style
     delete rest.className
     delete rest.default
+
+    // delete customAvatar url
+    delete rest.customAvatar
+    // return the customAvatar ReactNode
+    if (customAvatar !== '') {
+      return (
+        <img
+          alt="Gravatar for customAvatar"
+          style={this.props.style}
+          src={customAvatar}
+          height={this.props.size}
+          width={this.props.size}
+          {...rest}
+          className={className}
+        />
+      )
+    }
+
+
     if (!modernBrowser && isRetina()) {
       return (
         <img
